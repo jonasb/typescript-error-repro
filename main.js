a2d8dba6f7c1eee72c15b9d8d0d1f22305fe91ac
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-31 23:37:48 +0200

    fix

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_assert_1 = require("node:assert");
-(0, node_assert_1.equal)('Hello', 'He' + 'llo');
+import { equal } from 'node:assert';
+equal('Hello', 'He' + 'llo');
